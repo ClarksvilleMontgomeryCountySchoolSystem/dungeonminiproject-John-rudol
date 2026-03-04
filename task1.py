854d1d21eccf59f3e01971e560e43b9f394c02eb
John-rudol <johnathan.rudolph097@students.cmcss.net>
2026-03-04 07:40:20 -0600

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,6 +1,6 @@
 torch_lit = True
 if torch_lit:
-   outcome = "Flicker: The light shines!"
+   outcome = "Flicker: The light shall shine!"
 else:
-    outcome = "Doom: The light is out."
+    outcome = "Doom: The lights are out, we cannot see!"
 print(outcome)
